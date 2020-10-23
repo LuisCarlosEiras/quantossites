0ccf7a49e334a38a94a8c4c89d6b3db1e0389a4b
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-10-23 11:32:49 -0300

    Add files via upload

---
+++ Quantossites.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import streamlit as st
+
+from googletrans import Translator
+translator = Translator()
+
+st.title('Quantos sites e servidores tem a internet?')
+
+from bs4 import BeautifulSoup
+import requests
+url = "https://news.netcraft.com/archives/category/web-server-survey"
+r  = requests.get(url)
+data = r.text
+soup = BeautifulSoup(data, "lxml")
+container = soup.find("div",attrs={'class': 'post__content'})
+paragraph = container.find("p")
+for p in container.find_all("p", limit = 1):  
+    p.text
+    
+result=translator.translate(p.text, dest='pt') 
+st.write(result.text)
+
+st.write("""*Fonte*: Netcraft""")
+
+
+
+
+
+
+
