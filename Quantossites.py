2eadc78f2b6a65a85ccc0fd44f1b80a48b7cd699
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-12-02 06:23:52 -0300

    Update Quantossites.py

--- Quantossites.py
+++ Quantossites.py
@@ -26,7 +26,7 @@ for p in container.find_all("p", limit = 1):
     
 st.write("""*Tradução do googletrans*:""")
     
-result=translator.translate(p.text, dest='pt') 
+result=translator.translate(p.text, to_lang=to_lang) # dest='pt') 
 st.write(result.text)
 
 st.write("""*Fonte*: Netcraft""")
