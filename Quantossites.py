aae433ee8ba0f067623ddfaca32d8cd45575843e
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-12-03 07:55:56 -0300

    Update Quantossites.py

--- Quantossites.py
+++ Quantossites.py
@@ -31,14 +31,6 @@ from googletrans import Translator
 translator = Translator()
 
 st.write("""*Tradução do googletrans*:""") 
-result = translator.translate(p.text, dest = 'pt')
+result = translator.translate('p.text', dest = 'pt')
 st.write(result) 
 st.write("""*Fonte*: Netcraft""")
-
-
-
-
-
-
-
-
